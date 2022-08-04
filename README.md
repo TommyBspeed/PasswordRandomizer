2e74b8ef49d6075e7cd905f54bcfee875fd385ea
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-08-04 16:37:34 -0700

    added a pseudo text file to organize my thoughts as i go, also started adding some if statements to the js file and some small info to the readMe.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PasswordRandomizer
+
+## I am creating a password randomizer including prompts, alerts and confirm windows. These functions will set paramaters for what characters are to be used in the password as well as how long the password should be.
--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//create an array for lowercase letters
+var lower = [
+  "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z",
+];
+//create an array for numbers
+var numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"];
+//create an array for special characters
+var special = ["!, @, #, $, %, ^, &, *, -, +, =, <, >, ?"];
+//create an array for upper case letters
+var caps = [
+  "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z",
+];
+//create a final array with boolean selected arrays
+var finalArray = "";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,37 +29,76 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   //create initial prompt for passworld length
-  var passLength = prompt(
+  let passLength = prompt(
     "How many characters would you like in your password? Click OK to confirm."
   );
-  //create confirm window for special characters
-  var specialChars = confirm(
-    "Would you like to use special characters? Click OK to confirm."
-  );
-  //create confirm window for uppercase letters
-  var capLetters = confirm(
-    "Would you like to use upper case letters? Click OK to confirm."
-  );
-  //create confirm window for lowecase letters
-  var lowLetters = confirm(
-    "Would you like to use lower case letters? Click OK to confirm."
-  );
-  //create confirm window for numbers
-  var numbers = confirm("Would you like to use numbers? Click OK to confirm.");
+  //create if statements to set paramaters for min/max length
+  if (passLength < 8) {
+    alert("Sorry, your password needs to be a minimum of 8 characters.");
+    generatePassword();
+  } else if (passLength > 128) {
+    alert("Sorry, your password needs to be less than 128 characters.");
+    generatePassword();
+  } else {
+    //create confirm window for special characters
+    confirmSpecial = confirm(
+      "Would you like to use special characters? Click OK for yes, Cancel for no."
+    ); //create confirm window for uppercase letters
+    confirmCaps = confirm(
+      "Would you like to use upper case letters? Click OK for yes, Cancel for no."
+    ); //create confirm window for lowecase letters
+    confirmLower = confirm(
+      "Would you like to use lower case letters? Click OK for yes, Cancel for no."
+    ); //create confirm window for numbers
+    confirmNumbers = confirm(
+      "Would you like to use numbers? Click OK for yes, Cancel for no."
+    );
+  }
+  //create else if for 4 false booleans
+  if (!confirmSpecial && !confirmCaps && !confirmLower && !confirmNumbers) {
+    finalArray = alert("You must choose at least one criteria!");
+  }
 
-  //create an array for lowercase letters
-  lower = [
-    "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z",
-  ];
-  //create an array for numbers
-  numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"];
-  //create an array for special characters
-  special = ["!, @, #, $, %, ^, &, *, -, +, =, <, >, ?"];
-  //create an array for upper case letters
-  caps = [
-    "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z",
-  ];
+  //else if for 4 true booleans
+  else if (confirmSpecial && confirmCaps && confirmLower && confirmNumbers) {
+    finalArray = special.concat(caps, lower, numbers);
+  }
+  //else if for the 3 true booleans
+  else if (confirmSpecial && confirmCaps && confirmLower) {
+    finalArray = special.concat(caps, lower);
+  } else if (confirmSpecial && confirmCaps && confirmNumbers) {
+    finalArray = special.concat(caps, numbers);
+  } else if (confirmSpecial && confirmLower && confirmNumbers) {
+    finalArray = special.concat(lower, numbers);
+  } else if (confirmCaps && confirmLower && confirmNumbers) {
+    finalArray = caps.concat(lower, numbers);
+  }
 
+  //else if for 2 true booleans
+  else if (confirmSpecial && confirmCaps) {
+    finalArray = special.concat(caps);
+  } else if (confirmSpecial && confirmLower) {
+    finalArray = special.concat(lower);
+  } else if (confirmSpecial && confirmNumbers) {
+    finalArray = special.concat(numbers);
+  } else if (confirmLower && confirmNumbers) {
+    finalArray = lower.concat(numbers);
+  } else if (confirmLower && confirmCaps) {
+    finalArray = lower.concat(caps);
+  } else if (confirmNumbers && confirmCaps) {
+    finalArray = numbers.concat(caps);
+  }
+
+  //else if for single true boolean
+  else if (confirmSpecial) {
+    finalArray = special;
+  } else if (confirmCaps) {
+    finalArray = caps;
+  } else if (confirmLower) {
+    finalArray = lower;
+  } else if (confirmNumbers) {
+    finalArray = numbers;
+  }
   //return generated password
   return password;
 }
